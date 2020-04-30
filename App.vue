dfbb7c4c931a1d2396db1eb190bad240d127cfeb
Jensenbg-Henrik-Johansson <hj@users.noreply.github.com>
2020-04-30 14:10:36 +0200

    hhhh

---
+++ App.vue
@@ -0,0 +1,42 @@
+<template>
+    <section>
+        <generateInsult v-on:addNewInsult="addNewInsult"></generateInsult>
+        <ul v-for="shakeSpear in shakespear" :key="shakeSpear.insult">
+            <li> {{ shakeSpear.insult }} </li>
+         </ul>
+    </section>
+</template>
+
+<script>
+import generateInsult from './components/generateInsult.vue'
+
+export default {
+    components: {
+        generateInsult,
+    },
+    data: () => {
+        return {
+            shakespear: [
+                { play: 'Macbeth', insult: 'Were such things here as we do speak about? Or have we eaten on the insane root That takes the reason prisoner?'},
+                { play: 'Richard III', insult: 'Never hung poison on a fouler toad'},
+                { play: 'Julius Ceasar', insult: 'He thinks too much: such men are dangerous'},
+                { play: 'The Merchant of Venice', insult: 'Thou calledst me a dog before thou hadst a cause. But since I am a dog, beware my fangs'},
+                { play: 'The Two Noble Kinsmen', insult: 'Give me your hand...I can tell your fortune. You are a fool'},
+                { play: 'The Tempest', insult: 'He smells like a fish, a very ancient and fish-like smell, a kind of not-of-the-newest poor-John. A strange fish!'},
+                { play: 'Macbeth', insult: 'It is a tale Told by an idiot, full of sound and fury, Signifying nothing'},
+                { play: 'Titus Andronicus', insult: 'Alas, poor heart, that kiss is comfortless As frozen water to a starved snake'},
+                { play: 'Henry IV, Part 2', insult: 'He hath eaten me out of house and home; he hath put all substance into that fat belly of his'},
+                { play: 'Romeo and Juliet', insult: 'Out, you green-sickness carrion! Out, you baggage! You tallow-face!'}
+            ],
+        }
+    }, methods: {
+        addNewInsult: function(obj) {
+            this.shakespear.push(obj)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/generateInsult.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul>
+        <!--p> {{ randomInsult }} </p>
+        <button v-on:click="show">
+            get insult
+        </button>-->
+        <input v-model="play" placeholder="Play">
+        <input v-model="insult" placeholder="Insult">
+        <button v-on:click="newItem"> Create new insult </button>
+
+    </ul>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            play: "play",
+            insult: "insult",
+            object: {}
+        }
+    }, properties: {
+
+    },
+    methods: {
+        newItem: function() {
+            this.$emit("addNewInsult", {play: this.play, insult: this.insult})
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
